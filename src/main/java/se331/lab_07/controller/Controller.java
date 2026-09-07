6f63cf6ede0a69e3753cfe6534dfcb17e9885bda
YareDozo <thanawat10102548@gmail.com>
2026-09-07 13:43:57 +0700

    finished 3.2

--- src/main/java/se331/lab_07/controller/Controller.java
+++ src/main/java/se331/lab_07/controller/Controller.java
@@ -90,8 +90,7 @@ public class Controller {
     }
 
     @GetMapping("/events")
-    public ResponseEntity<List<Event>> getEventLists(@RequestParam(value = "_limit",
-                                                             required = false) Integer perPage
+    public ResponseEntity<List<Event>> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
             , @RequestParam(value = "_page", required = false) Integer page) {
         perPage = perPage == null ? eventsList.size() : perPage;
         page = page == null ? 1 : page;
@@ -108,7 +107,7 @@ public class Controller {
     }
 
     @GetMapping("/events/{id}")
-    public ResponseEntity<Event> getEvent(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
         Event output = null;
         for (Event event : eventsList) {
             if (event.getId().equals(id)) {
